ebe3d3a8b639a624f17df6829b51b485253854aa
Sahil Randhawa <sahildeep.randhawa@gmail.com>
2025-01-28 00:12:28 -0500

    initial setup

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "example-log-analytics"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Terraform Azure Log Analytics Workspace
+
+This module creates an Azure Log Analytics Workspace.
