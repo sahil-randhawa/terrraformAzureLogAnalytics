6768a1e00492ba01103cbd78362792f3f68b085b
Sahil Randhawa <sahildeep.randhawa@gmail.com>
2025-02-10 23:57:24 -0500

    workflow testing

---
+++ extras/terraform/main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+output "hello_world" {
+  value = "Hello, World!"
+}
